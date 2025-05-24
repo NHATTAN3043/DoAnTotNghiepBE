07375e304af9d056b89a600390a54f5c7ffacfa7
NNTanNextcore <tannn@nextcore.vn>
2025-05-24 11:17:53 +0700

    update: delete schedule

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,3 +7,5 @@ INSERT INTO "NGUOIDUNG" (
 ) VALUES (
              'Nguyen Van A', 'admin@gmail.com','$2a$12$ye0EjxA.tdq38/.xevjeA..m4KVpy.a9NE77oXR.7flLgzed7E7x.', 1
          );
+
+CREATE EXTENSION IF NOT EXISTS unaccent;
