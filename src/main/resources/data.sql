d571fe1b3c4528b75158c17485f808bf4ed97f41
NHATTAN3043 <nhattan3043@gmail.com>
2025-06-28 23:04:04 +0700

    delete redis

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,7 +5,7 @@ INSERT INTO "CHUCVU" ("maChucVu", "tenChucVu") VALUES (3, 'Manager');
 INSERT INTO "NGUOIDUNG" (
     "ten", "email", "matKhau", "maChucVu"
 ) VALUES (
-             'Nguyen Van A', 'admin@gmail.com','$2a$12$ye0EjxA.tdq38/.xevjeA..m4KVpy.a9NE77oXR.7flLgzed7E7x.', 1
+             'Nguyen Van A', 'admin@gmail.com','$2a$12$ye0EjxA.tdq38/.xevjeA..m4KVpy.a9NE77oXR.7flLgzed7E7x.', 3
          );
 
 CREATE EXTENSION IF NOT EXISTS unaccent;
